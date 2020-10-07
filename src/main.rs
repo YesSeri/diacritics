692e0d18cdfaf2d74fe48c65a7fc60fd62f23123
yesseri <henrik.zenkert@gmail.com>
2020-10-08 00:01:00 +0200

    Working diacritics remover, and small tests.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,66 @@
-struct ReplacePair<'a> {
-    base: &'a str,
-    diacritics: &'a str,
-}
 fn main() {
-    let replace_pairs = create_pairs();
-    let word = "TÅR";
-    for i in word.chars(){
-        println!("{}",i);
-    }
-    for a in replace_pairs {
-        println!("{},  {}", a.base, a.diacritics);
-    }
+    let string: &str = "TÅRÖÄÆØ";
+    println!("{}", string);
+    let new_string = remove_diacritics(&string);
+    println!("{}", new_string);
 }
 
-fn create_pairs<'a>() -> Vec<ReplacePair<'a>> {
-    let mut vector = Vec::new();
-    vector.push(ReplacePair {
-        base: "A",
-        diacritics: "Å",
-    });
-    vector
-}
-fn chars(s: &str) -> &str {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..i];
-        }
+fn remove_diacritics(string: &str) -> String {
+    let v = string.chars();
+    let mut split_string: Vec<String> = Vec::new();
+    for c in v {
+        split_string.push(String::from(c));
     }
+    let new_string: String = split_string
+        .iter()
+        .map(|j| match j.as_ref() {
+            "À" | "Á" | "Â" | "Ã" | "Ä" | "Å" | "Æ" => "A",
+            "Þ" => "B",
+            "Ç" | "Č" => "C",
+            "Ď" | "Ð"  => "D",
+            "Ě" | "È" | "É" | "Ê" | "Ë" => "E",
+            "Ƒ" => "F",
+            "Ì" | "Í" | "Î" | "Ï" => "I",
+            "Ň" | "Ñ" => "N",
+            "Ò" | "Ó" | "Ô" | "Õ" | "Ö" | "Ø" => "O",
+            "Ř" =>"R",
+            "ß" => "ss",
+            "Š" => "S",
+            "Ť" => "T",
+            "Ů" | "Ù" | "Ú" | "Û" | "Ü" => "U",
+            "Ý" => "Y",
+            "Ž" => "Z",
 
-    &s[..]
+            "à" | "á" | "â" | "ã" | "ä" | "å" | "æ" => "a",
+            "þ"=> "b",
+            "ç" | "č" => "c",
+            "ď" | "ð"  => "d",
+            "ě" | "è" | "é" | "ê" | "ë" => "e",
+            "ƒ" => "f",
+            "ì" | "í" | "î" | "ï" => "i",
+            "ñ" | "ň" => "n",
+            "ò" | "ó" | "ô" | "õ" | "ö" | "ø" => "o",
+            "ř" => "r",
+            "š" => "s",
+            "ť" => "t",
+            "ů" | "ù" | "ú" | "û" | "ü" => "u",
+            "ý" | "ÿ" => "y",
+            "ž" => "z",
+
+            _ => j,
+        })
+        .collect();
+    new_string
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_uppercase(){
+        assert_eq!(remove_diacritics("TÅRÖÄÆØ"), String::from("TAROAAO"))
+    }
+    #[test]
+    fn test_lowercase(){
+        assert_eq!(remove_diacritics("čďêƒíó"), String::from("cdefio"))
+    }
 }
