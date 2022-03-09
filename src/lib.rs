41dd7694867832274c33d2e475c4d9c782a8cd69
YesSeri <34012616+YesSeri@users.noreply.github.com>
2022-03-09 20:49:09 +0100

    more rust-like

--- src/lib.rs
+++ src/lib.rs
@@ -11,49 +11,45 @@
 ///
 
 pub fn remove_diacritics(string: &str) -> String {
-    let v = string.chars();
-    let mut split_string: Vec<String> = Vec::new();
-    for c in v {
-        split_string.push(String::from(c));
-    }
-    split_string
-        .iter()
-        .map(|j| match j.as_ref() {
-            "À" | "Á" | "Â" | "Ã" | "Ä" | "Å" | "Æ" => "A",
-            "Þ" => "B",
-            "Ç" | "Č" => "C",
-            "Ď" | "Ð" => "D",
-            "Ě" | "È" | "É" | "Ê" | "Ë" => "E",
-            "Ƒ" => "F",
-            "Ì" | "Í" | "Î" | "Ï" => "I",
-            "Ň" | "Ñ" => "N",
-            "Ò" | "Ó" | "Ô" | "Õ" | "Ö" | "Ø" => "O",
-            "Ř" => "R",
-            "ß" => "ss",
-            "Š" => "S",
-            "Ť" => "T",
-            "Ů" | "Ù" | "Ú" | "Û" | "Ü" => "U",
-            "Ý" => "Y",
-            "Ž" => "Z",
+    let chars = string.chars();
+    chars.fold("".to_string(), |acc, c| acc + &find_char_match(c))
+}
+fn find_char_match(c: char) -> String {
+    match c {
+        'À' | 'Á' | 'Â' | 'Ã' | 'Ä' | 'Å' | 'Æ' => "A".to_string(),
+        'Þ' => "B".to_string(),
+        'Ç' | 'Č' => "C".to_string(),
+        'Ď' | 'Ð' => "D".to_string(),
+        'Ě' | 'È' | 'É' | 'Ê' | 'Ë' => "E".to_string(),
+        'Ƒ' => "F".to_string(),
+        'Ì' | 'Í' | 'Î' | 'Ï' => "I".to_string(),
+        'Ň' | 'Ñ' => "N".to_string(),
+        'Ò' | 'Ó' | 'Ô' | 'Õ' | 'Ö' | 'Ø' => "O".to_string(),
+        'Ř' => "R".to_string(),
+        'ß' => "ss".to_string(),
+        'Š' => "S".to_string(),
+        'Ť' => "T".to_string(),
+        'Ů' | 'Ù' | 'Ú' | 'Û' | 'Ü' => "U".to_string(),
+        'Ý' => "Y".to_string(),
+        'Ž' => "Z".to_string(),
 
-            "à" | "á" | "â" | "ã" | "ä" | "å" | "æ" => "a",
-            "þ" => "b",
-            "ç" | "č" => "c",
-            "ď" | "ð" => "d",
-            "ě" | "è" | "é" | "ê" | "ë" => "e",
-            "ƒ" => "f",
-            "ì" | "í" | "î" | "ï" => "i",
-            "ñ" | "ň" => "n",
-            "ò" | "ó" | "ô" | "õ" | "ö" | "ø" => "o",
-            "ř" => "r",
-            "š" => "s",
-            "ť" => "t",
-            "ů" | "ù" | "ú" | "û" | "ü" => "u",
-            "ý" | "ÿ" => "y",
-            "ž" => "z",
-            _ => j,
-        })
-        .collect()
+        'à' | 'á' | 'â' | 'ã' | 'ä' | 'å' | 'æ' => "a".to_string(),
+        'þ' => "b".to_string(),
+        'ç' | 'č' => "c".to_string(),
+        'ď' | 'ð' => "d".to_string(),
+        'ě' | 'è' | 'é' | 'ê' | 'ë' => "e".to_string(),
+        'ƒ' => "f".to_string(),
+        'ì' | 'í' | 'î' | 'ï' => "i".to_string(),
+        'ñ' | 'ň' => "n".to_string(),
+        'ò' | 'ó' | 'ô' | 'õ' | 'ö' | 'ø' => "o".to_string(),
+        'ř' => "r".to_string(),
+        'š' => "s".to_string(),
+        'ť' => "t".to_string(),
+        'ů' | 'ù' | 'ú' | 'û' | 'ü' => "u".to_string(),
+        'ý' | 'ÿ' => "y".to_string(),
+        'ž' => "z".to_string(),
+        _ => c.to_string(),
+    }
 }
 #[cfg(test)]
 mod tests {
